8bbe78e750f118486c012e805084d87a03a29efa
noahvaks <noahvaks@gmail.com>
2020-11-11 01:32:56 -0800

    navigation implemented
    TO DO:
    initialize pages
    show/hides
    enable/disables

--- qt/app/mainwindow.cpp
+++ qt/app/mainwindow.cpp
@@ -8,13 +8,78 @@ MainWindow::MainWindow(QWidget *parent)
 {
 	ui->setupUi(this);
 	DBManager::instance();
-
-    ui->pushButton_pages_home->setDisabled(true);
-
 }
 
 MainWindow::~MainWindow()
 {
 	delete ui;
 }
+
+/*----NAVIGATION----*/
+void MainWindow::on_pushButton_pages_home_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(HOME);
+}
+
+void MainWindow::on_pushButton_pages_view_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(VIEW);
+}
+    void MainWindow::on_pushButton_view_search_clicked()
+    {
+        ui->stackedWidget_view_pages->setCurrentIndex(SEARCH);
+    }
+
+    void MainWindow::on_pushButton_view_list_clicked()
+    {
+        ui->stackedWidget_view_pages->setCurrentIndex(LIST);
+    }
+
+void MainWindow::on_pushButton_pages_plan_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(PLAN);
+}
+
+    void MainWindow::on_pushButton_plan_continue_clicked()
+    {
+        ui->stackedWidget_pages->setCurrentIndex(POS);
+    }
+
+    void MainWindow::on_pushButton_pos_cancel_clicked()
+    {
+        on_pushButton_pages_home_clicked();
+    }
+
+    void MainWindow::on_pushButton_pos_continue_clicked()
+    {
+        ui->stackedWidget_pages->setCurrentIndex(RECEIPT);
+    }
+
+    void MainWindow::on_pushButton_receipt_continue_clicked()
+    {
+        on_pushButton_pages_home_clicked();
+    }
+
+void MainWindow::on_pushButton_pages_admin_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(LOGIN);
+}
+
+    void MainWindow::on_pushButton_login_clicked()
+    {
+        ui->stackedWidget_pages->setCurrentIndex(ADMIN);
+    }
+
+    void MainWindow::on_pushButton_admin_import_clicked()
+    {
+        ui->stackedWidget_admin_pages->setCurrentIndex(IMPORT);
+    }
+
+    void MainWindow::on_pushButton_admin_edit_clicked()
+    {
+        ui->stackedWidget_admin_pages->setCurrentIndex(EDIT);
+    }
+/*----END NAVIGATION----*/
+
+
 
--- qt/app/mainwindow.h
+++ qt/app/mainwindow.h
@@ -17,7 +17,62 @@ public:
 	MainWindow(QWidget *parent = nullptr);
 	~MainWindow();
 
+private slots:
+    /*----NAVIGATION----*/
+    void on_pushButton_pages_home_clicked();
+
+    void on_pushButton_pages_view_clicked();
+
+        void on_pushButton_view_search_clicked();
+
+        void on_pushButton_view_list_clicked();
+
+    void on_pushButton_pages_plan_clicked();
+
+        void on_pushButton_plan_continue_clicked();
+
+        void on_pushButton_pos_cancel_clicked();
+
+        void on_pushButton_pos_continue_clicked();
+
+        void on_pushButton_receipt_continue_clicked();
+
+    void on_pushButton_pages_admin_clicked();
+
+        void on_pushButton_login_clicked();
+
+        void on_pushButton_admin_import_clicked();
+
+        void on_pushButton_admin_edit_clicked();
+    /*----END NAVIGATION----*/
+
+
 private:
+    /*----NAVIGATION ENUMS----*/
+    enum Pages
+    {
+        HOME,
+        VIEW,
+        PLAN,
+        POS,
+        RECEIPT,
+        LOGIN,
+        ADMIN
+    };
+
+    enum ViewPages
+    {
+        SEARCH,
+        LIST
+    };
+
+    enum AdminPages
+    {
+        IMPORT,
+        EDIT
+    };
+    /*----END NAVIGATION ENUMS----*/
+
 	Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
